e445cfe9559fe664b0bfc5698a0fd2b49c123f11
Chabi chakraborty <149655837+chabi69420@users.noreply.github.com>
2026-04-14 22:30:40 +0530

    Add: permutation-string - submission-0

---
+++ Data Structures & Algorithms/permutation-string/submission-0.java
@@ -0,0 +1,48 @@
+class Solution {
+    public boolean checkInclusion(String s1, String s2) {
+
+        if (s1.length() > s2.length()) {
+            return false;
+        }
+
+        int[] count1 = new int[26];
+        int[] count2 = new int[26];
+
+        // Frequency for s1 and first window of s2
+        for (int i = 0; i < s1.length(); i++) {
+            count1[s1.charAt(i) - 'a']++;
+            count2[s2.charAt(i) - 'a']++;
+        }
+
+        // Check first window
+        if (matches(count1, count2)) {
+            return true;
+        }
+
+        // Sliding window
+        for (int i = s1.length(); i < s2.length(); i++) {
+
+            // Add new character
+            count2[s2.charAt(i) - 'a']++;
+
+            // Remove old character
+            count2[s2.charAt(i - s1.length()) - 'a']--;
+
+            // Compare
+            if (matches(count1, count2)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    private boolean matches(int[] arr1, int[] arr2) {
+        for (int i = 0; i < 26; i++) {
+            if (arr1[i] != arr2[i]) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
